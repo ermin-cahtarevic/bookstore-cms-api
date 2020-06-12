e0c3530a1b2fd9895412f90f72406620ecff3748
Ermin Cahtarevic <erminc@windowslive.com>
2020-06-12 18:53:15 +0200

    Add create and show methods to the books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,6 +2,24 @@ module Api
   module V1
     class BooksController < ApplicationController
 
+      def create
+        @book = Book.new(book_params)
+
+        if @book.save
+          render json: {
+            status: 'SUCCESS',
+            message: 'Book created',
+            data: @book
+          }, status: :ok
+        else
+          render json: {
+            status: 'ERROR',
+            message: 'Book creation failed',
+            data: @book.errors
+          }, status: :unprocessable_entity
+        end
+      end
+
       def index
         @books = Book.all
 
@@ -11,6 +29,22 @@ module Api
           data: @books
         }, status: :ok
       end
+
+      def show
+        @book = Book.find(params[:id])
+
+        render json: {
+          status: 'SUCCESS',
+          message: 'Loaded book',
+          data: @book
+        }, status: :ok
+      end
+
+      private
+
+        def book_params
+          params.permit(:title, :category)
+        end
     end
   end
 end
