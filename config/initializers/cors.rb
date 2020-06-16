31294287663f7578cc08dbd7298a6cc5550aaa50
Ermin Cahtarevic <erminc@windowslive.com>
2020-06-16 18:59:04 +0200

    Update url in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://bookstore-rails-api.herokuapp.com/'
+    origins 'https://react-bookstore-ermin.herokuapp.com/'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
